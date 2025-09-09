990b3801f2cd0b0acbe77632d7405754e4a0ff1d
sbt80171 <sbt80171@gmail.com>
2025-09-09 16:50:11 +0900

    :art: change UI

--- normalize.py
+++ normalize.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 FORM = 'NFKC'
 
-SAMPLE = '例：１２３ ①②③ ＡＢＣ ﾃｷｽﾄ ㍻ ㌔ ㈱ '
+SAMPLE = '１２３ ①②③ ＡＢＣ ﾃｷｽﾄ ㍻ ㌔ ㈱ '
 
 def main():
     st.set_page_config(
@@ -14,11 +14,11 @@ def main():
     )
     st.title('Normalize')
     st.write('テキストのunicode正規化アプリ')
+    st.caption(f"例：「{SAMPLE}」 -> 「{unicodedata.normalize(FORM, SAMPLE)}」")
 
     col_from, col_to = st.columns(2, border=True)
     with col_from:
-        st.write(':material/Check: テキストを入力してください')
-        st.caption(f"{SAMPLE}")
+        st.write(':memo: テキストを入力してください')
         initial_height = 100
         text = st.text_area(
             label='_',
@@ -28,24 +28,15 @@ def main():
 
     with col_to:
         st.write(':sparkles: unicode正規化したテキスト')
-        st.caption(f"{unicodedata.normalize(FORM, SAMPLE)}")
         content = ''
         if not text:
             st.code(content, language=None)
         elif unicodedata.is_normalized(FORM, text):
-            st.write('正規化されたテキストです')
+            st.info('すでに正規化されたテキストです')
         else:
             content = unicodedata.normalize(FORM, text)
             st.code(content, language=None)
 
-    col_left, col_right = st.columns(2)
-    with col_left:
-        st.markdown("""
-        * ブラウザ更新でリセットできます
-        * NFKC 形式で正規化しています
-        * unicode正規化の詳細については [Unicode正規化](https://ja.wikipedia.org/wiki/Unicode%E6%AD%A3%E8%A6%8F%E5%8C%96) 等でご確認ください
-        """)
-    with col_right:
         st.download_button(
             label='Download',
             data=content,
@@ -53,6 +44,12 @@ def main():
             on_click='ignore',
             disabled=not content
         )
+
+    st.markdown("""
+    * ブラウザ更新でリセットできます
+    * NFKC 形式で正規化しています
+    * unicode正規化の詳細については [Unicode正規化](https://ja.wikipedia.org/wiki/Unicode%E6%AD%A3%E8%A6%8F%E5%8C%96) 等でご確認ください
+    """)
 
 
 if __name__ == '__main__':
